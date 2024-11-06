bc90a780280e5fd5967b1cca0304e41f67455666
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-11-06 12:32:06 +0600

    EF Edit and LINQ

--- IntroEF/IntroEF/Controllers/StudentController.cs
+++ IntroEF/IntroEF/Controllers/StudentController.cs
@@ -31,16 +31,44 @@ namespace IntroEF.Controllers
         public ActionResult Create() { 
             return View();
         }
+        /*public ActionResult Details(string id) { 
+            (from s in db.Students
+            where s.Name.Contains(id)
+            select s).FirstOrDefault();
+        }*/
         public ActionResult Details(int id) {
             var data = db.Students.Find(id);
             if (data != null) {
                 return View(data);
             }
             TempData["Msg"] = "Student with Id "+id+" not found";
-            return RedirectToAction("List");
-            
-            
+            return RedirectToAction("List");   
         }
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var data = db.Students.Find(id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student formObj) {
+            //
+            //
+            
+            var exobj = db.Students.Find(formObj.Id);
+            formObj.Cgpa = exobj.Cgpa;
+            db.Entry(exobj).CurrentValues.SetValues(formObj);
+            //Recommended One
+            //exobj.Name = formObj.Name;
+            //exobj.Cgpa = formObj.Cgpa;
+
+
+            db.SaveChanges();
+            TempData["Msg"] = "Student Updated";
+            return RedirectToAction("List");
+        }
+
+        //to remove 
+        // db.Students.Remove(exobj)
         
     }
 }
