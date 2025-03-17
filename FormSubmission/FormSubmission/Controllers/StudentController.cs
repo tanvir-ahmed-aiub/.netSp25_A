ced62d4d948714d25b8e05cf2e630e29d6d0c26b
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-03-17 10:44:36 +0600

    Form Submission

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult Index() {
+            return View(new Student()) ;
+        }
+        [HttpPost]
+        public ActionResult Index(Student s)
+        {
+            if (ModelState.IsValid) {
+                return RedirectToAction("About","Home");
+            }
+            return View(s);
+        }
+        //[HttpPost]
+        //public ActionResult Index(string Name, string Id, string Email) {
+        //    ViewBag.Name = Name;
+        //    ViewBag.Email = Email;
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc)
+        //{
+        //    ViewBag.Name = Request["Name"];
+        //    ViewBag.Email = fc["Email"];
+        //    return View();
+        //}
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide Id")]
+        [Range(1,40)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must not exceed 100 characters")]
+        public string Name { get; set; }
+        [Required,StringLength(10)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+}
