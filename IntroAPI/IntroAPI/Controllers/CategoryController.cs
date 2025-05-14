2de60f5564dee72fdce4e0d2873abfa4faf465f4
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-05-14 11:57:32 +0600

    Intro API

---
+++ IntroAPI/IntroAPI/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using IntroAPI.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        PMS_Sp25_AEntities db = new PMS_Sp25_AEntities();
+        [HttpGet]
+        [Route("api/category/all")]
+        public HttpResponseMessage AllCats() {
+            var data = db.Categories.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK,data);
+        }
+        [HttpGet]
+        [Route("api/category/{id}/name/{name}")]
+        public HttpResponseMessage Category(int id, string name) {
+            var data = db.Categories.Find(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpPost]
+        [Route("api/category/create")]
+        public HttpResponseMessage Create(Category c) {
+            c.CreatedBy = 99;
+            c.CreatedAt = DateTime.Now;
+            db.Categories.Add(c);
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.Created,"Category Created");
+        }
+    }
+}
---
+++ IntroAPI/IntroAPI/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public HttpResponseMessage Get() {
+
+
+
+            return Request.CreateResponse(HttpStatusCode.OK,"Get Called");
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Get Called" +id );
+        }
+
+        public HttpResponseMessage Post() {
+            return Request.CreateResponse(HttpStatusCode.OK, "Post Called");
+        }
+        public HttpResponseMessage Delete() {
+            return Request.CreateResponse(HttpStatusCode.OK, "Delete Called");
+        }
+    }
+}
